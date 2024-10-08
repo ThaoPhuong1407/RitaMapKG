f130c57930b0df07c804047cc230e65a655bc902
Thao Phuong <thao@dollabs.com>
2024-10-08 09:24:56 -0400

    add direction and additional info to the relationship

--- src/rita_map_kg/core.clj
+++ src/rita_map_kg/core.clj
@@ -19,29 +19,31 @@
   (init-rmq-instance config) ;; Initialize RMQ instance
   
   ;; Read and extract nodes and relationships from file
-  (let [file-path "./resources/dragon_2.0.json" 
+  (let [file-path "./resources/dragon_2.0.json"
         data (extract-schema file-path)
         nodes (:nodes data)
         relationships (:relationships data)]
 
     ;; Send nodes to RMQ
     (doseq [node nodes]
-      (send-rmq-message config
-                        {:state "create-node" 
-                         :label (:label node)
-                         :properties (:properties node)}))
+      (let [node-data (second node)]
+        (send-rmq-message config
+                          {:state "create-node"
+                           :label (:label node-data)
+                           :properties (:properties node-data)})))
 
     ;; Send relationships to RMQ
     (doseq [rel relationships]
-      (send-rmq-message config
-                        {:state "create-relationship" 
-                         :start_property_key "nodeid"
-                         :start_property_value (:source rel) 
-                         :end_property_key "nodeid"
-                         :end_property_value (:target rel)       
-                         :relationship_type (:relationship_type rel)
-                         :relationship_properties (:properties rel)})))
-                         
+      (let [rel-data (second rel)]
+        (send-rmq-message config
+                          {:state "create-relationship"
+                           :start_property_key "nodeid"
+                           :start_property_value (:source rel-data)
+                           :end_property_key "nodeid"
+                           :end_property_value (:target rel-data)
+                           :relationship_type (:relationship_type rel-data)
+                           :relationship_properties (:properties rel-data)}))))
+
   ;; Close the RMQ connection
   (close-rmq-connection)
   (println "Messages sent and RMQ connection closed."))
--- src/rita_map_kg/schema.clj
+++ src/rita_map_kg/schema.clj
@@ -5,18 +5,37 @@
 (defn take-first-part [s]
   (first (str/split s #"_")))
 
+(defn get-coordinates [mymap key]
+  (get-in mymap [key :properties :coordinates]))
+
+
+(defn compare-areas [coords1 coords2]
+  (let [[x1 z1 _ _ _ _] coords1  
+        [x3 z3 _ _ _ _] coords2  
+        x-diff (- x3 x1)
+        z-diff (- z3 z1)
+        x-dir (cond
+                (pos? x-diff) "E"
+                (neg? x-diff) "W"
+                :else nil)
+        z-dir (cond
+                (pos? z-diff) "S"
+                (neg? z-diff) "N"
+                :else nil)]
+    (str (when z-dir (str z-dir)) 
+         (when x-dir (str x-dir)))))
+
 ;; Function to process areas and extract nodes
 (defn process-area [id details]
   (let [{:keys [initial]} details
         {:keys [args pclass]} initial
         coordinates (take 6 args)
         name (utils/clean-name (nth args 6)) 
-        ;; Extract field-ref and the list of connection names
-        field-ref (nth args 7 nil)
-        connections (if (and (map? field-ref) (= ":field-ref" (:type field-ref)))
-                      (:names field-ref)
-                      [])] ;; Extract connections from :field-ref if present
-    ;; Return the node with connections
+        ;; Collect all field-ref connections
+        connections (->> args
+                         (filter map?) ;; Filter only maps in the args
+                         (filter #(= ":field-ref" (:type %))) 
+                         (mapcat :names))] ;; Extract all :names and flatten the list
     {:label (take-first-part pclass) 
      :properties {:nodeid id
                   :coordinates coordinates
@@ -24,49 +43,50 @@
                   :connections connections}}))
 
 ;; Function to create relationships based on connection info
-(defn process-connection-info [id details data]
+(defn process-connection-info [details nodes]
   (let [{:keys [args pclass]} (:initial details)
         coordinates (take 6 args)
         lvar (nth args 7)]
     (when (map? lvar)
       (let [{:keys [type names]} lvar]
-        (when (= type ":field-ref")
-          (map (fn [name]
-                 (let [{:keys [source target]} (utils/extract-source-target (utils/clean-name name))
-                       ;; Base relationship map
-                       relationship {:source source 
-                                     :target target
-                                     :relationship_type pclass
-                                     :properties {:id name 
-                                                  :coordinates coordinates
-                                                  }}]
-                   relationship))
-               names))))))
+        (when (and (= type ":field-ref") (seq names))
+          (let [name (first names)
+                {:keys [source target]} (utils/extract-source-target (utils/clean-name name))
+                source-cord (get-coordinates nodes (keyword source))
+                target-cord (get-coordinates nodes (keyword target)) source-direction (compare-areas target-cord source-cord)
+                target-direction (compare-areas source-cord target-cord)
+                relationship {:source source 
+                              :target target
+                              :relationship_type pclass
+                              :properties {:id name 
+                                           :coordinates coordinates
+                                           :is-bidirectional true
+                                           :description (str source-direction " " pclass " " target-direction)}}]
+            relationship))))))
+
 
 ;; Main function to extract nodes and relationships from the entire file
 (defn extract-schema [file-path]
   (let [data (utils/read-file file-path)
-        relationships (atom [])
-        nodes (atom [])
+        relationships (atom {})
+        nodes (atom {})
         connection-pclasses #{"Door" "DoubleDoor" "OpeningDoor" "Extension" "Cliff" "CorridorJoin"}]
 
-    ;; First, process nodes (do not create relationships here)
+    ;; First, process nodes 
     (doseq [[id details] data]
       (when (and (:initial details)
                  (:args (:initial details))
                  (not (contains? connection-pclasses (:pclass (:initial details)))))
-        ;; Process and collect node data
-        (swap! nodes conj (process-area id details))))
+        (swap! nodes assoc id (process-area id details)))) 
 
     ;; Then, process relationships from valid connection entries
-    (doseq [[id details] data]
+    (doseq [[_id details] data]
       (when (and (:initial details)
                  (contains? connection-pclasses (:pclass (:initial details))))
-        (let [relationship-info (process-connection-info id details data)]
-          (doseq [relationship relationship-info]
-            (when relationship ;; Ensure no nils
-              (swap! relationships conj relationship))))))
+        (let [relationship (process-connection-info details @nodes)]
+            (when relationship ;; Ensure no nils  
+              (swap! relationships assoc (:id (:properties relationship)) relationship)))))
 
-    ;; Return extracted nodes and relationships
+    ;; Return extracted nodes and relationships 
     {:nodes @nodes
-     :relationships (distinct @relationships)}))
+     :relationships @relationships}))
